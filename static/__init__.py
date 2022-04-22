5474a1ac72f43e90ed1e9d93e26eda023097ae10
Hitesh.Balegar <hitesh.balegar@betsol.com>
2022-04-22 11:26:24 +0530

    refactored __init__.py

--- static/__init__.py
+++ static/__init__.py
@@ -17,6 +17,7 @@ cors = CORS()
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iifzaxouzngflp:072c008481eb606222b53e50bafae3d1890cc4aef01df54a47f537f4fc85bf41@ec2-52-54-212-232.compute-1.amazonaws.com:5432/d4ormdorhkn74r'
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from static.todoApp.model.todo_list_model import Todo
     db.init_app(app)
@@ -24,7 +25,6 @@ def create_app(test_config=None):
     cors.init_app(app, resource={r"/api/*": {"origins": "*"}})
 
     from static.todoApp import todo_list
-
     app.register_blueprint(todo_list, url_prefix='/api/v1')
 
     return app
